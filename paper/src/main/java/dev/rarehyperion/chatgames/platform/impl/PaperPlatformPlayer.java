e574a91cc43dda5f5e78d3d0ca6976c7c540bead
Foxx09p <foxx09p@gmail.com>
2026-04-06 08:01:23 -0500

    Update PaperPlatformPlayer.java

--- paper/src/main/java/dev/rarehyperion/chatgames/platform/impl/PaperPlatformPlayer.java
+++ paper/src/main/java/dev/rarehyperion/chatgames/platform/impl/PaperPlatformPlayer.java
@@ -2,8 +2,8 @@ package dev.rarehyperion.chatgames.platform.impl;
 
 import dev.rarehyperion.chatgames.platform.PlatformPlayer;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import org.bukkit.entity.Player;
-
 import java.util.UUID;
 
 public class PaperPlatformPlayer implements PlatformPlayer {
@@ -29,5 +29,8 @@ public class PaperPlatformPlayer implements PlatformPlayer {
         return this.player.getUniqueId();
     }
 
-
+    @Override
+    public void showTitle(final Title title) {
+        this.player.showTitle(title);
+    }
 }
