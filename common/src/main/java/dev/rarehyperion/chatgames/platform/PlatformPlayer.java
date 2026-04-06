2f7cdf8ca393a56ce9cc8648f6fabe20373e05e3
Foxx09p <foxx09p@gmail.com>
2026-04-06 08:00:07 -0500

    Update PlatformPlayer.java

--- common/src/main/java/dev/rarehyperion/chatgames/platform/PlatformPlayer.java
+++ common/src/main/java/dev/rarehyperion/chatgames/platform/PlatformPlayer.java
@@ -1,37 +1,12 @@
 package dev.rarehyperion.chatgames.platform;
 
 import net.kyori.adventure.text.Component;
-
+import net.kyori.adventure.title.Title;
 import java.util.UUID;
 
-/**
- * Represents a player on the platform.
- *
- * <p>
- *     Provides methods for sending messages, and accessing the player identity.
- * </p>
- *
- * @author RareHyperIon
- * @since 20/12/2025
- */
 public interface PlatformPlayer {
-
-    /**
-     * Sends a chat message to the player.
-     * @param component The message to send.
-     */
     void sendMessage(final Component component);
-
-    /**
-     * Returns the name of the player.
-     * @return The player's name.
-     */
     String name();
-
-    /**
-     * Returns the unique identifier (UUID) of the player.
-     * @return The player's UUID.
-     */
     UUID id();
-
+    void showTitle(final Title title);
 }
